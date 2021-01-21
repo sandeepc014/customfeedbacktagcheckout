4d3c168f143e6d9d5100c3ceddeed1e823cc2b7a
sandeep kousik <sandeepc@linkstreet.in>
2021-01-21 10:55:00 +0530

    tag removed in t2.sh

--- tagcheckout2.sh
+++ tagcheckout2.sh
@@ -2,24 +2,26 @@
 
 cd /var/www/html/moodle-stage.linkstreet.in/blocks/customfeedback
 
-thetagname=$(sudo git tag | tail -1)
 
-echo "The current tag is $thetagname"
-echo ""
-
-sudo git fetch -a
-
-latesttagname=$(sudo git tag | tail -1)
-
-echo "Fetching new tag"
-
-echo $latesttagname
-echo ""
-echo ""
-
-sudo git checkout $latesttagname
+echo "Pulling code from Master Branch"
 
 echo ""
 echo ""
-echo "Successfully Deployed $latesttagname in https://moodle-stage.linkstreet.in site"
+echo ""
+
+sudo git pull origin master 
+
+echo ""
+echo ""
+
+echo "The latest five commits are below"
+
+echo ""
+echo ""
+
+git log -n5
+
+echo ""
+echo ""
+echo "Successfully Deployed code in https://moodle-stage.linkstreet.in site"
 
